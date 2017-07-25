9e1ab160777469a7c0cefedbd5f5b2c807f1101c
dylan-gregory <jdylangregory@gmail.com>
2017-07-25 16:35:22 -0400

    Working with jCarousel plugin, tweaking carousel

--- main.js
+++ main.js
@@ -6,6 +6,41 @@
 
 
 
+    $('.jcarousel').jcarousel();
+
+    console.log($);
+
+    // var next = function(){
+    //   jcarouselControl({
+    //     target: '+=1',
+    //   })
+    // };
+    //
+    // $('.jcarousel-next').on('click', next);
+
+    $('.jcarousel-control-prev')
+     .on('jcarouselcontrol:active', function() {
+         $(this).removeClass('inactive');
+     })
+     .on('jcarouselcontrol:inactive', function() {
+         $(this).addClass('inactive');
+     })
+     .jcarouselControl({
+         target: '-=1'
+     });
+
+
+
+     $('.jcarousel-control-next')
+      .on('jcarouselcontrol:active', function() {
+          $(this).removeClass('inactive');
+      })
+      .on('jcarouselcontrol:inactive', function() {
+          $(this).addClass('inactive');
+      })
+      .jcarouselControl({
+          target: '+=1'
+      });
 
 
 
